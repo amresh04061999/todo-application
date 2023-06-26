2950d7c5f9fc1dacd9c6773b3235e4898ae5f60d
Amresh Prasad <amresh1098@gmail.com>
2023-06-26 18:06:08 +0530

    feat:add

--- src/app/todo/to-do-container/to-do-presentation/to-do-presentation.component.ts
+++ src/app/todo/to-do-container/to-do-presentation/to-do-presentation.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./to-do-presentation.component.scss']
 })
 export class ToDoPresentationComponent {
+  all = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  active = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  resolved = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  close = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+ public  isInputVisible:boolean;
+ constructor(){
+this.isInputVisible=false
+ }
+  drop(event: CdkDragDrop<string[]>) {
+    console.log();
+   
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+  public openInputBox(){
 
+  }
+
+
+  toggleInputVisibility() {
+    this.isInputVisible = !this.isInputVisible;
+  }
 }
